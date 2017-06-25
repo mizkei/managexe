8e43d693b8d339a20234fd88607dff36e6e23669
mizkei <m.1111.keita@gmail.com>
2017-06-26 01:05:45 +0900

    add comment Task interface

--- task.go
+++ task.go
@@ -4,18 +4,23 @@ import (
 	"context"
 )
 
+// Task is task interface.
 type Task interface {
 	Run(context.Context) error
 }
 
+// TaskInserter is the interface that implements insert task.
 type TaskInserter interface {
 	InsertTask(context.Context, Task) error
 }
 
+// TaskFetcher is the interface that implements fetch task.
+// FetchTask should block until fetch a task.
 type TaskFetcher interface {
 	FetchTask(context.Context) (Task, error)
 }
 
+// Queue is the inteface that implements TaskInserter and TaskFetcher.
 type Queue interface {
 	TaskInserter
 	TaskFetcher
